b8bcd6e098a6b942228c2fc46e44e16aa8de5268
nayu <ose794@gamil.com>
2025-03-16 02:25:08 +0900

    フレンド検索画面追加！可読性ゴミ！

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stu/add_friend.dart';
 import 'my_home_page.dart';
 
 void main() {
@@ -6,15 +7,21 @@ void main() {
 }
 
 // このウィジェットはアプリのルート（最上位）です。
-class MyApp extends StatelessWidget {//継承
+class MyApp extends StatelessWidget {
+  //継承
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(),
+
+      //home: const MyHomePage(),
+      initialRoute: "/", //画面遷移path登録.initialRoute:が起動時の画面指定
+      routes: <String, WidgetBuilder>{
+        "/": (BuildContext context) => const MyHomePage(),
+        "/add_friend": (BuildContext context) => const AddFriend(),
+      },
     );
   }
 }
-
---
+++ lib/timer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class timer extends StatefulWidget {
+  const timer({super.key});
+
+  @override
+  State<timer> createState() => _timer();
+}
+
+
+// State クラス（画面の状態を管理）
+class _timer extends State<timer> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
